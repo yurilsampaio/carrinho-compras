bb6a4ae57a6d3b2ddd840da263a441a61f5d1728
Yuri Sampaio <yurillsampaio@gmail.com>
2025-02-12 00:49:17 -0300

    alterações na grid
    agora tem um componente especifico para mostrar cada item de maneira mais organizada e atraente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './styles.css'
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
-import { Button, Table } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import AddItemModal from "./components/AddItemModal/AddItemModal";
+import CartItem from './components/CartItem/CartItem';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -50,42 +51,25 @@ function App() {
           <Button variant='success' onClick={() => setShowModal(true)}>Adicionar Item</Button> 
         </div>
         {items.length > 0 && (
-        <div>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Item</th>
-                <th>Vlr. UN</th>
-                <th>Qtd.</th>
-                <th>Vlr. Total</th>
-              </tr>
-            </thead>
-            <tbody>
+          <div>
+            <div className='cart-items'>
               {items.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.name}</td>
-                  <td>R$ {item.price.toFixed(2)}</td>
-                  <td>{item.quantity}</td>
-                  <td>R$ {item.total.toFixed(2)}</td>
-                  <td>
-                    <Button variant="danger" onClick={() => handleRemoveItem(index)}>
-                      <i className='bi bi-trash'></i>
-                    </Button>
-                  </td>
-                </tr>
+                <CartItem
+                  key={index}
+                  item={item}
+                  onRemove={() => handleRemoveItem(index)}
+                />
               ))}
-            </tbody>
-          </Table>
-        
-          <div className='fixed-bottom'>
-            <div className='d-flex justify-content-center my-2'>
-                <p>Total: R$ {totalPrice.toFixed(2)}</p>
             </div>
-            <div className='d-flex justify-content-center'>
-              <Button variant='success' onClick={handleClearCart}>Finalizar Compra</Button>
+            <div className='fixed-bottom'>
+              <div className='d-flex justify-content-center my-2'>
+                  <p>Total: R$ {totalPrice.toFixed(2)}</p>
+              </div>
+              <div className='d-flex justify-content-center'>
+                <Button variant='success' onClick={handleClearCart}>Finalizar Compra</Button>
+              </div>
             </div>
           </div>
-        </div>
         )}
       </main>
       <AddItemModal show={showModal} handleClose={() => setShowModal(false)} addItem={handleAddItem} />
---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './styles.css'
+
+const CartItem = ({ item, onRemove }) => {
+    return (
+        <div className="cart-item">
+            <div className="item">
+                <div className="item-details">
+                    <span className="item-name">{item.name}</span>
+                    <div className="item-info">
+                        <span className="item-price">Vlr. Un. R$ {item.price.toFixed(2)}</span>
+                        <span className="item-quantity">Qtd. {item.quantity}</span>
+                    </div>
+                </div>
+                <span className="item-total">R$ {item.total.toFixed(2)}</span>
+            </div>
+            <Button variant="danger" onClick={onRemove}>
+                <i className="bi bi-trash"></i>
+            </Button>
+        </div>
+    );
+};
+
+export default CartItem;
